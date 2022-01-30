19f1343f814b999b1654b1de0d639b3320798db1
FMinister <johannes.leyrer+git@googlemail.com>
2022-01-30 22:10:57 +0100

    frontend finished

--- frontend/src/components/tickets/Ticket.jsx
+++ frontend/src/components/tickets/Ticket.jsx
@@ -2,13 +2,16 @@ import Card from "../ui/Card";
 import classes from "./Ticket.module.css";
 
 const Ticket = (props) => {
+    console.log(props);
     return (
         <li className={classes.item}>
             <Card>
                 <div className={classes.content}>
                     <h3>{props.title}</h3>
-                    <section>{props.section}</section>
-                    <p>{props.issue}</p>
+                    <section>Bereich: {props.section}</section>
+                    <p>Problembeschreibung: {props.issue}</p>
+                    <p>Timestamp: {props.timestamp}</p>
+                    <p>Anhänge: {props.attachments}</p>
                 </div>
             </Card>
         </li>
--- frontend/src/components/tickets/TicketsList.jsx
+++ frontend/src/components/tickets/TicketsList.jsx
@@ -2,19 +2,25 @@ import Ticket from "./Ticket";
 import classes from "./TicketsList.module.css";
 
 const TicketsList = (props) => {
-    return (
-        <ul className={classes.list}>
-            {props.tickets.map((ticket) => (
-                <Ticket
-                    key={ticket.id}
-                    id={ticket.id}
-                    title={ticket.title}
-                    section={ticket.section}
-                    issue={ticket.issue}
-                />
-            ))}
-        </ul>
-    );
+    if (props.tickets.length > 0) {
+        return (
+            <ul className={classes.list}>
+                {props.tickets.map((ticket) => (
+                    <Ticket
+                        key={ticket.id}
+                        id={ticket.id}
+                        title={ticket.title}
+                        section={ticket.section}
+                        issue={ticket.issue}
+                        timestamp={ticket.timestamp}
+                        attachments={ticket.attachments}
+                    />
+                ))}
+            </ul>
+        );
+    }
+
+    return <p>Keine Tickets verfügbar.</p>;
 };
 
 export default TicketsList;
